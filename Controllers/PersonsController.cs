5c6f6cbe2892cfd85a196dd940b25ad509c2158e
Mosab Al Sherawy <mosabzx@hotmail.com>
2021-06-07 02:11:34 +0200

    model view update

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -13,45 +13,76 @@ namespace ASP.NET_MVC_Assignments.Controllers
     {
         public PersonsModel person;
 
-        public PersonsController(PersonsModel Person)
+
+        static List<PersonsModel> PeopleList = new List<PersonsModel>
         {
-            this.person = Person;
+            new PersonsModel
+            {
+                 Id = 1, Name = "Mosab", City = "Halmstad" , Phone = 0737771507
+            },
+            new PersonsModel
+            {
+                Id = 2, Name = "Mario", City = "Stockholm" , Phone = 1234567890
+            },
+            new PersonsModel
+            {
+                Id = 3, Name = "Zoro", City = "Helsingborg" , Phone = 098432345
+            },
+        };
+
+        public PersonsController(PersonsModel person)
+        {
+            this.person = person;
+            
             
         }
 
+        
 
 
         // GET: PersonsController
         public ActionResult Index()
         {
-            
-            person.Peoples();
-            var persons = person.List();
+
+            var persons = PeopleList;
+
             return View(persons);
         }
+        
+
 
         // GET: PersonsController/Details/5
         public ActionResult Details(int id)
         {
-            person.Peoples();
-            var persons = person.Find(id);
+            
+            var persons = PeopleList.SingleOrDefault(p => p.Id == id);
             return View(persons);
         }
+
+
+
+
 
         // GET: PersonsController/Create
         public ActionResult Create()
         {
+
             return View();
         }
 
         // POST: PersonsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(PersonsModel persons)
         {
             try
             {
+                
+                PeopleList.Add(persons);
+
+               
                 return RedirectToAction(nameof(Index));
+
             }
             catch
             {
@@ -59,40 +90,47 @@ namespace ASP.NET_MVC_Assignments.Controllers
             }
         }
 
-        // GET: PersonsController/Edit/5
-        public ActionResult Edit(int id)
+
+
+
+
+        //Get:
+        public ActionResult Filter()
         {
+
             return View();
         }
 
-        // POST: PersonsController/Edit/5
+        //Post:
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Filter(string filter)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+           var result = PeopleList.Where(p => p.Name.Contains(filter) || p.City.Contains(filter)).ToList();
+            
+
+            return View(result);
         }
+
 
         // GET: PersonsController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+           var persons = PeopleList.SingleOrDefault(p => p.Id == id);
+          
+            return View(persons);
         }
 
         // POST: PersonsController/Delete/5
         [HttpPost]
         //[ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id,PersonsModel persons)
         {
             try
             {
+                
+                var person = PeopleList.SingleOrDefault(p => p.Id == id);
+                PeopleList.Remove(person);
+                
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -100,5 +138,47 @@ namespace ASP.NET_MVC_Assignments.Controllers
                 return View();
             }
         }
+
+
+
+
+
+
+
+
+
+
+        //// GET: PersonsController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: PersonsController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- Models/PersonsModel.cs
+++ Models/PersonsModel.cs
@@ -12,70 +12,70 @@ namespace ASP.NET_MVC_Assignments.Models
         public string City { get; set; }
         public int Phone { get; set; }
 
-         List<PersonsModel> PeopleList;
 
-        public PersonsModel()
-        {
-            
-            
-
-        }
+        static List<PersonsModel> PeopleList;
+        
 
 
-        public void Peoples()
-        {
-            PeopleList = new List<PersonsModel>()
-            {
-                new PersonsModel
-                {
-                     Id = 1, Name = "Mosab", City = "Halmstad" , Phone = 0737771507
-                },
-                new PersonsModel
-                {
-                    Id = 2, Name = "Mario", City = "Stockholm" , Phone = 1234567890
-                },
-                new PersonsModel
-                {
-                    Id = 3, Name = "Tahoo", City = "Oskarström" , Phone = 0987654321
-                },
 
-            };
-            
 
-        }
+
+
+        
 
 
         public List<PersonsModel> List()
         {
 
-            return PeopleList;
+            return PersonsModel.PeopleList;
         }
 
+
+
+
+
+        public void Add(PersonsModel person)
+        {
+            
+            PersonsModel.PeopleList.Add(person);
+            
+           
+        }
+
+       
         public PersonsModel Find(int id)
         {
-            var person = PeopleList.SingleOrDefault(p => p.Id == id);
+            var person = PersonsModel.PeopleList.SingleOrDefault(p => p.Id == id);
 
             return person;
         }
 
         public PersonsModel Filter(string text,PersonsModel person)
         {
-            PeopleList.Where (p => p.Name.Contains(text.ToLowerInvariant()));
-            PeopleList.Where(p => p.City.Contains(text.ToLowerInvariant()));
+            PersonsModel.PeopleList.Where (p => p.Name.Contains(text.ToLowerInvariant()));
+            PersonsModel.PeopleList.Where(p => p.City.Contains(text.ToLowerInvariant()));
 
             return person;
         }
 
-        public void Add(PersonsModel person)
-        {
-            PeopleList.Add(person);
-            
-        }
+        
 
         public void Erase (int id)
         {
             var person = Find(id);
-            PeopleList.Remove(person);
+            PersonsModel.PeopleList.Remove(person);
+
+            //PeopleList.Remove(Find(id));
+
+            //foreach (var item in PeopleList)
+            //{
+            //    if (item == person)
+            //    {
+            //        PeopleList.Remove(item);
+            //    }
+            //}
+
+
         }
 
 
